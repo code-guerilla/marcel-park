27ce26a5d54551e42e7dd6d64c2f6870063b0557
code-guerilla <marcel_park@gmx.de>
2026-01-25 17:03:12 +0100

    feat: Introduce Knip for dependency analysis, update dependencies, remove blog data processing, and refine configurations.

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,11 +4,3 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-
-export function formatDate(date: string) {
-  return new Date(date).toLocaleDateString("de-DE", {
-    year: "numeric",
-    month: "2-digit",
-    day: "2-digit",
-  })
-}
